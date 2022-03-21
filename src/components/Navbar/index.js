4df3af661930204d2b3f071296a57803230b57ae
IRaihan97 <1820557@brunel.ac.uk>
2022-03-21 15:52:05 +0000

    First Commit with Nav

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+    Nav,
+    NavLink,
+    Bars,
+    NavMenu,
+    NavBtn,
+    NavBtnLink
+  } from './NavbarElements';
+
+const Navbar = () => {
+  return (
+      <>
+        <Nav>
+            <NavLink to="/">
+                <h1>LOGO</h1>
+            </NavLink>
+            <Bars/>
+            <NavMenu>
+                <NavLink to="/page1" activeStyle>
+                    Page 1
+                </NavLink>
+                <NavLink to="/page2" activeStyle>
+                    Page 2
+                </NavLink>
+                <NavLink to="/page3" activeStyle>
+                    Page 3
+                </NavLink>
+            </NavMenu>
+            <NavBtn>
+                <NavBtnLink to="/signin">Sign In</NavBtnLink>
+            </NavBtn>
+        </Nav>
+      </>
+  )
+}
+
+export default Navbar
