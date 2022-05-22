a96fc488bf0e7ff77f43fd5a2a061f365463ab40
IRaihan97 <1820557@brunel.ac.uk>
2022-05-22 12:55:09 +0100

    Completed header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import './header.css';
 import { Element } from 'react-scroll'
+import {Link} from 'react-scroll'
 
 const Header = () => {
   return (
     <React.Fragment>
       <Element id='header-section' name='header-section'>
         <div className="BTM__header" id="BTM__header">
-          <div className="BTM__header_BG"/>
+          <div className="BTM__header_BG">
+            <div className="header__overlay">
+              <div className="header__content">
+                <p>Fostering symbiotic 
+                    relationships between
+                    industry and academia 
+                    through innovative IT & Data
+                    Science services delivered by our
+                    leading experts and 
+                    talented students. </p>
+                <Link
+                activeClass='active'
+                to="about-section"
+                spy={true}
+                smooth={true}
+                duration={500}>
+                  <button>Click Here for More</button>
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
       </Element>
     </React.Fragment>
