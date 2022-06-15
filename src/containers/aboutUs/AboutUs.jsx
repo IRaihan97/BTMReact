76382b16b1f988753d68dff1e787a3754e62087a
IRaihan97 <1820557@brunel.ac.uk>
2022-06-15 11:38:36 +0100

    Added About Us Animation

--- src/containers/aboutUs/AboutUs.jsx
+++ src/containers/aboutUs/AboutUs.jsx
@@ -5,6 +5,8 @@ import bgImg from '../../assets/about/aboutUsHeader.png'
 import bridgeImg from '../../assets/about/bridge.png'
 import eyeImg from '../../assets/about/eye.png'
 import { Element } from 'react-scroll'
+import { AnimationOnScroll } from 'react-animation-on-scroll';
+import 'animate.css/animate.min.css'
 
 
 const paddingTop = 250;
@@ -26,18 +28,31 @@ const AboutUs = () => {
           <h2 style={{color: "white", fontSize: 30, margin: 20}}>Watch Our Testimonials</h2>
           <Slideshow/>
         </div>
-        <div className='BTM__statements'>
-          <div className='BTM__statements_overlay'>
-            <div className='BTM__statement_content'>
-              <h1>Our Mission</h1>
-              <p>To bridge the gap between industry and academia through innovative IT & Data Science services. Unlike most of our competition, we are not a profit-driven organisation. Rather, we are a marketplace, that aims to foster symbiotic relations between our customers and the university. We are distinguishable by our bank of both talented students as well as staff-leading specialists.</p>
-            </div>
-            <div className='BTM__statement_content'>
-              <h1>Our Vision</h1>
-              <p>To become a recognisable and viable IT consultancy known for its Research & Development services for West London businesses. We aspire to give 2% of the computer department’s student cohort the opportunity to participate in BTM projects. We aim to expand this model to other university departments, such as design, business management, and mechanical engineering.  </p>
+        
+        <AnimationOnScroll 
+          animateIn='animate__fadeIn'
+          animateOnce='true'>
+          <div className='BTM__statements'>
+            <div className='BTM__statements_overlay'>
+            <AnimationOnScroll 
+                animateIn='animate__zoomIn'
+                animateOnce='true'>
+              <div className='BTM__statement_content'>
+                <h1>Our Mission</h1>
+                <p>To bridge the gap between industry and academia through innovative IT & Data Science services. Unlike most of our competition, we are not a profit-driven organisation. Rather, we are a marketplace, that aims to foster symbiotic relations between our customers and the university. We are distinguishable by our bank of both talented students as well as staff-leading specialists.</p>
+              </div>
+            </AnimationOnScroll>
+            <AnimationOnScroll 
+                animateIn='animate__zoomIn'
+                animateOnce='true'>
+              <div className='BTM__statement_content'>
+                <h1>Our Vision</h1>
+                <p>To become a recognisable and viable IT consultancy known for its Research & Development services for West London businesses. We aspire to give 2% of the computer department’s student cohort the opportunity to participate in BTM projects. We aim to expand this model to other university departments, such as design, business management, and mechanical engineering.  </p>
+              </div>
+            </AnimationOnScroll>
             </div>
           </div>
-        </div>
+        </AnimationOnScroll>
         {/* <ContentHolder bgImg={bridgeImg} title={<h1>Our Mission</h1>} content={<div style={{color: "white", fontSize: 30}}><p>To bridge the gap between industry and academia through innovative IT & Data Science services. Unlike most of our competition, we are not a profit-driven organisation. Rather, we are a marketplace, that aims to foster symbiotic relations between our customers and the university. We are distinguishable by our bank of both talented students as well as staff-leading specialists.</p></div>} topP={paddingTop} bottomP={paddingBot}/>
         <ContentHolder bgImg={eyeImg} title={<h1>Our Vision</h1>} content={<div style={{color: "white", fontSize: 30}}><p>To become a recognisable and viable IT consultancy known for its Research & Development services for West London businesses. We aspire to give 2% of the computer department’s student cohort the opportunity to participate in BTM projects. We aim to expand this model to other university departments, such as design, business management, and mechanical engineering.  </p></div>} topP={paddingTop} bottomP={paddingBot}/> */}
       </div>
