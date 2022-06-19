94a54febaab6968944ac40ac1552177f26aa29b0
IRaihan97 <1820557@brunel.ac.uk>
2022-06-19 16:15:22 +0100

    Added Youtube Player

--- src/containers/util/slideshow/Slideshow.jsx
+++ src/containers/util/slideshow/Slideshow.jsx
@@ -4,6 +4,8 @@ import 'react-awesome-slider/dist/styles.css';
 import './slideshow.css'
 import Mandeep from '../../../assets/experts/mandeepbanner.png'
 import Phillip from '../../../assets/experts/phillipbanner.png'
+import ReactPlayer from 'react-player';
+import YouTubePlayer from 'react-player/lib/players/YouTube';
 
 
 const Slideshow = () => {
@@ -11,14 +13,24 @@ const Slideshow = () => {
     <div className='slider__holder'>
         <AwesomeSlider>
             <div className='testimonial__slides'>
-                <a href='https://youtu.be/B8Qi0zCwnTg'>
+                {/* <a href='https://youtu.be/B8Qi0zCwnTg'>
                     <img src={Mandeep} width="100%" height="100%"/>
-                </a>
+                </a> */}
+                <ReactPlayer
+                    url="https://youtu.be/B8Qi0zCwnTg"
+                    height={"90%"}
+                    width={"80%"}
+                />
             </div>
             <div className='testimonial__slides'>
-                <a href='https://youtu.be/kvqfa1wolj0'>
+                {/* <a href='https://youtu.be/kvqfa1wolj0'>
                     <img src={Phillip} width="100%" height="100%"/>
-                </a>
+                </a> */}
+                <ReactPlayer
+                    url="https://youtu.be/kvqfa1wolj0"
+                    height={"90%"}
+                    width={"80%"}
+                />
             </div>
         </AwesomeSlider>
     </div>
